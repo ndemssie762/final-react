8ab1c9bad3e35b55814dfbdadcb06b47611cf542
nathan102030 <79672895+nathan102030@users.noreply.github.com>
2024-04-26 23:13:27 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+import Forecast from "./components/forecast/forecast";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import "./App.css";
+function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (responses) => {
+        const [weatherResponse, forecastResponse] = await Promise.all(responses.map(response => response.json()));
+
+        if (weatherResponse && forecastResponse) {
+          setCurrentWeather({ city: searchData.label, ...weatherResponse });
+          setForecast({ city: searchData.label, ...forecastResponse });
+        } else {
+          console.error("Unexpected response format");
+        }
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,16 @@
+ 
+  const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "c925435284msh0629bb825b11fb4p1ba5bdjsne043386b47cc",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "e841f27489e6c8f5519b4196f105389b"; // enter your key from openweather API
+ 
+ 
+
